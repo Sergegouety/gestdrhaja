94da3f63bd9d60fb5a967e152e93307d0af336e8
Serge Gouety <sergekrystel@gmail.com>
2023-06-02 16:53:10 +0000

    reset password

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -20,7 +20,7 @@
   <!-- Custom styles for this template -->
   <link href="{{asset('css/style.css')}}" rel="stylesheet">
   <link href="{{asset('css/style-responsive.css')}}" rel="stylesheet">
-  
+
   <!-- =======================================================
     Template Name: Dashio
     Template URL: https://templatemag.com/dashio-bootstrap-admin-template/
@@ -40,11 +40,11 @@
       <span style="font-size: 22px; color:#00a650;"><strong>BIENVENUE SUR L'APPLICATION DE GESTION <br>DE LA DRHAJA DE L'AGENCE EMPLOI JEUNES</strong></span>
     </div>
       <form autocomplete="off" class="form-login" method="POST" action="{{ route('password.update') }}">
-         {{ csrf_field() }} 
+         {{ csrf_field() }}
         <h2 class="form-login-heading">Réinitialiser votre mot de passe</h2>
 
         <input type="hidden" name="token" value="{{ $token }}">
-        
+
         <div class="login-wrap">
         <!--   <input type="email" name="email" class="form-control" placeholder="mail" autofocus>
           <br> -->
@@ -52,20 +52,49 @@
            @error('email')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
+
                 </span>
             @enderror
-            <br><strong><input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Nouveau mot de passe">
+            <br><strong>
+                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Nouveau mot de passe" onkeyup="validatePassword(this.value)">
+                <br/>
+                <span style="display: block; color:red" id="non_majuscule">
+                        <input type="checkbox" ><label>Au moins une lettre Majuscule</label>
+                </span>
+                <span style="display: none; color:green" id="majuscule">
+                        <input type="checkbox"  checked><label>Au moins une lettre Majuscule</label>
+                </span>
+                <br/>
+                <span style="display: block; color:red" id="non_chiffre">
+                        <input type="checkbox"><label>Au moins 1 chiffre</label>
+                </span>
+                <span style="display: none; color:green" id="chiffre">
+                        <input type="checkbox"  checked><label>Au moins 1 chiffre</label>
+                </span>
+                <br/>
+                <span style="display: block; color:red" id="non_longueur">
+                        <input type="checkbox"><label>Au moins 8 carractères</label>
+                </span>
+                <span style="display: none; color:green" id="longueur">
+                        <input type="checkbox"  checked><label>Au moins 8 carractères</label>
+                </span>
+
+
             @error('password')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
-            @enderror</strong>
+            @enderror
+        </strong>
 
             <br>
-            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Confirmer mot de passe">
-          
+            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Confirmer mot de passe" onkeyup="activeSubmit()">
+            <span style="display: none; color:red" id="pwdrepeat">
+             <label>Repetez le mot de passe svp !</label>
+        </span>
+
           <hr>
-          <button type="submit" class="btn" style="background-color: #f8941e; color: white">
+          <button type="submit" class="btn" style="background-color: #f8941e; color: white" disabled id="btn_submit">
             {{ __('Réinitialiser votre mot de passe') }}
         </button>
           <hr>
@@ -73,17 +102,17 @@
           <div style="text-align: center;">
          <img src="{{asset('img/logo_aej.png')}}" class="img-circle" width="120">
          </div>
-          
+
         </div>
-        
+
       </div>
-        
-  
+
+
       </form>
-      
+
     </div>
   </div>
-  
+
   <!-- js placed at the end of the document so the pages load faster -->
   <script src="{{asset('lib/jquery/jquery.min.js')}}"></script>
   <script src="{{asset('lib/bootstrap/js/bootstrap.min.js')}}"></script>
@@ -94,6 +123,34 @@
     $.backstretch("{{asset('img/login-bg.jpg')}}", {
       speed: 500
     });
+
+    function validatePassword(password) {
+
+
+                                         // Au moins une lettre majuscule
+                                        if (!/[A-Z]/.test(password)) { $('#majuscule').css("display","none");$('#non_majuscule').css("display","block");}else{$('#majuscule').css("display","block");$('#non_majuscule').css("display","none");}
+
+                                        // Au moins un chiffre
+                                        if (!/\d/.test(password)) { $('#chiffre').css("display","none");$('#non_chiffre').css("display","block");}else{$('#chiffre').css("display","block");$('#non_chiffre').css("display","none");}
+
+                                         //Minimum 8 caractères
+                                        if (password.length < 8) { $('#longueur').css("display","none");$('#non_longueur').css("display","block");}else{$('#longueur').css("display","block");$('#non_longueur').css("display","none");}
+
+                                        if (!/^(?=.*[A-Z])(?=.*\d).{8,}$/.test(password)) { $('#password-confirm').attr('disabled','true')}else{ $('#password-confirm').removeAttr('disabled');}
+
+
+                                        }
+
+function activeSubmit() {
+//alert('test');
+                        var password_confirmation = $('#password-confirm').val();
+                        var password_new = $('#password').val();
+
+                        if(password_confirmation == password_new){$('#btn_submit').removeAttr('disabled'); $('#pwdrepeat').css("display","none");}
+
+                       }
+
+
   </script>
 </body>
 
